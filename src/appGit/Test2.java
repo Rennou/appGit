993535b55c0c2a4cb72fd9cda308ed8f6f643431
Rennou <taous.ren@gmail.com>
2019-10-13 15:56:00 -0600

    Revert "cOMMIT apres git stash"
    This reverts commit 07db5a0358a3018a80ecba03fd8c3aeab557c530.

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -3,7 +3,11 @@ package appGit;
 public class Test2 {
 	
 public void test() {	
+<<<<<<< HEAD
 System.out.println("111ccccc111");
 System.out.println("222");}
 
+=======
+System.out.println("111111");}
+>>>>>>> parent of 07db5a0... cOMMIT apres git stash
 }
