e363557dca2ebf3b4be6ed37871b81e7192ac11c
Rennou <taous.ren@gmail.com>
2019-10-13 15:59:34 -0600

    test de commit

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -4,10 +4,14 @@ public class Test2 {
 	
 public void test() {	
 <<<<<<< HEAD
+<<<<<<< HEAD
 System.out.println("111ccccc111");
 System.out.println("222");}
 
 =======
 System.out.println("111111");}
 >>>>>>> parent of 07db5a0... cOMMIT apres git stash
+=======
+System.out.println("111ccccc111");}
+>>>>>>> parent of be0c755... mmit pour  tester commit revert
 }
