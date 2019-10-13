6016da3eb74d4820f04fc800bb3580a6a23588a6
Rennou <taous.ren@gmail.com>
2019-10-13 16:00:50 -0600

    Revert "mmit pour  tester commit revert"
    This reverts commit be0c755bae0263d179fda8539ac919f4efc5a74b.

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -3,15 +3,7 @@ package appGit;
 public class Test2 {
 	
 public void test() {	
-<<<<<<< HEAD
-<<<<<<< HEAD
 System.out.println("111ccccc111");
 System.out.println("222");}
 
-=======
-System.out.println("111111");}
->>>>>>> parent of 07db5a0... cOMMIT apres git stash
-=======
-System.out.println("111ccccc111");}
->>>>>>> parent of be0c755... mmit pour  tester commit revert
 }
