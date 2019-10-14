16dce8b4a7e235e80acbd5e7281e7bbc13c1807b
Rennou <taous.ren@gmail.com>
2019-10-13 19:22:01 -0600

    commit master

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -4,6 +4,7 @@ public class Test2 {
 	
 public void test() {	
 System.out.println("111ccccc111");
-System.out.println("2211+++++112");}
+System.out.println("2211+++++112");
+System.out.println("master");}
 
 }
