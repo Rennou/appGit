2c1348f2ffae66ac746b500e87bb61bebb384159
Rennou <taous.ren@gmail.com>
2019-10-14 10:38:51 -0600

    tester le rebase

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -7,7 +7,8 @@ System.out.println("111ccccc111");
 System.out.println("2211134");
 System.out.println("dev");
 System.out.println("2211+++++112");
-System.out.println("master");}
+System.out.println("master");
+System.out.println("dev2");}
 
 
 }
