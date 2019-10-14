04003f9f1378177d5e51b49b180174496a3bcb5a
Rennou <taous.ren@gmail.com>
2019-10-13 19:20:24 -0600

    commit dev

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -4,6 +4,8 @@ public class Test2 {
 	
 public void test() {	
 System.out.println("111ccccc111");
-System.out.println("2211112");}
+System.out.println("2211112");
+System.out.println("dev");
+}
 
 }
