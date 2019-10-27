d0c7953f579585767557df886c58d4e12c6ef481
Rennou <taous.ren@gmail.com>
2019-10-27 17:01:02 -0600

    coriger conflit

--- src/appGit/Test.java
+++ src/appGit/Test.java
@@ -7,6 +7,11 @@ public class Test {
       System.out.println("Tes123t");
       System.out.println("123333");
       System.out.println("123333444");
+      System.out.println("123");
+      System.out.println("345");
+      System.out.println("678");
+      System.out.println("999");
+
 
 	}
 
