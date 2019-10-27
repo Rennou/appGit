9068ec922b4658ec8eb14ecee5b6575afa4cc060
Rennou <taous.ren@gmail.com>
2019-10-27 12:54:15 -0600

    commit0001 :ajouter un commit ds la branche test

--- src/appGit/Test2.java
+++ src/appGit/Test2.java
@@ -6,5 +6,6 @@ public void test() {
 System.out.println("111ccccc111");
 System.out.println("2211+++++112");
 System.out.println("master");
-System.out.println("master2");}
+System.out.println("master2");
+System.out.println("test");}
 }
